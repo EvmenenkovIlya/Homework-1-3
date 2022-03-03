609da806b1e0e3bd0babfe421b88a81c830e9a85
Ilya <Evmenya@gmail.com>
2022-03-03 19:09:25 +0300

    Creation BranchStructures Class

---
+++ HW 1-3/BranchStructuries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HW_1_3
+{
+    internal class BranchStructuries
+    {
+        /* Пользователь вводит 2 числа (A и B). Если A>B, подсчитать A+B, 
+         * если A=B, подсчитать A*B, если A<B, подсчитать A-B.*/
+        public static int FindSumOrDifferenceOrProduct(int a, int b)
+        {
+            if (a > b)
+            {
+                return (a + b);
+            }
+            else if (a == b)
+            {
+                return (a * b);
+            }
+            else
+            {
+                return (a - b);
+            }
+        }
+
+
+
+
+    }
+}
--- HW 1-3/Program.cs
+++ HW 1-3/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using HW_1_3;
-string s = Variablies.FindEquationsByTwoPoints(1, 1, 2, 3);
-Console.WriteLine(s);
+﻿using HW_1_3;
+
+
+Console.WriteLine(BranchStructuries.FindSumOrDifferenceOrProduct(1,2));
